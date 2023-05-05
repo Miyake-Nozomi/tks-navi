8b526942549ce5fe040ceca92073e9fb80c5b48a
nagare-2 <122423416+nagare-2@users.noreply.github.com>
2023-05-05 14:47:08 +0900

    こども食堂とは
    こども食堂とは

--- page-concept.php
+++ page-concept.php
@@ -1,13 +1,15 @@
 <?php get_header(); ?>
 
 <main>
-    <div class="main_inner mt">
+    <div class="main_inner">
         <?php get_template_part('template-parts/breadcrumb'); ?>
-        <div class="concept">
+        <section class="concept">
             <h2 class="title">こども食堂とは</h2>
+            <div class="textbox">
             <div class="text">
                 <p>
-                    こども食堂と聞くと、”貧困層のための場所”や、”こどもや子育て世代向けのもの”といったイメージを抱く方も多いと思います。実際はそうではなく、こどもの為の場であることはもちろんですが、地域の人や大人、様々な世代の人が集まって、一緒にごはんを食べて交流できる場です。地域とのつながりや、多世代交流をする地域コミュニティとしての役割も担っています。
+                    こども食堂と聞くと、”貧困層のための場所”や、”こどもや子育て世代向けのもの”といったイメージを抱く方も多いと思います。
+                    実際はそうではなく、こどもの為の場であることはもちろんですが、地域の人や大人、様々な世代の人が集まって、一緒にごはんを食べて交流できる場です。地域とのつながりや、多世代交流をする地域コミュニティとしての役割も担っています。
                 </p>
             </div>
             <div class="text">
@@ -20,6 +22,8 @@
                     こども食堂は、子どもたちの健やかな成長を支援するために、全国各地で展開されています。
                 </p>
             </div>
+            <div class="curry"></div>
+            </div>
             <!-- <div class="btn"> -->
             <div class="btn_item">
                 <a href="<?php echo home_url('/faq'); ?>">
@@ -27,11 +31,8 @@
                 </a>
             </div>
             <!-- </div> -->
-            <img src="<?php echo get_template_directory_uri();?>/assets/images/child/rice.png" alt="背景"
-                class="concept_img rice">
-            <img src="<?php echo get_template_directory_uri();?>/assets/images/child/curry.png" alt="背景"
-                class="concept_img curry">
-        </div>
+            
+        </section>
     </div>
 </main>
 
