cea9e04f9a080b2618afb7b1e1cf1e3497f66511
nozaki08 <uryahoooi89@gmail.com>
2023-05-06 15:12:10 +0900

    食堂運営者のページ
    食堂運営者のページ修正
    
    野崎

--- page-admin.php
+++ page-admin.php
@@ -3,19 +3,20 @@
     <div class="main_inner">
         <div class="block"></div>
         <?php get_template_part('template-parts/breadcrumb'); ?>
-        <section class="section_inner">
+        <section class="section_inner admin_inner">
             <h2 class="title">食堂運営者の方へ</h2>
             <p class="text">
-                「徳島こども食堂ナビ」では、徳島県下で活動されている、こども食堂さんであれば、所属に関わらずどこでも無料で情報掲載が可能です。
+                「徳島こども食堂ナビ」では、徳島県下で活動されている、こども食堂さんであれば、所属に関わらずどこでも<span
+                    class="highlight fontweight">無料で情報掲載が可能</span>です。
             </p>
             <p class="text">
-                このサイトは、"こども食堂を利用したい人、手伝いたい人、支援したい人"へ情報をお届けすることを目的としています。<br>
+                このサイトは、 <span class="highlight">"こども食堂を利用したい人、手伝いたい人、支援したい人"へ情報をお届けすることを目的</span> としています。<br>
                 徳島県下の、こども食堂の活動がより活発に行われる事を願って、有志ボランティアにより運営されています。
             </p>
             <p class="text">
                 情報掲載希望の食堂運営者の方は、<a href="<?php echo home_url('/contact/'); ?>">【お問い合わせ】</a>からご連絡ください。
             </p>
-            <p class="text ">
+            <p class="text">
                 こども食堂紹介ページに、こども食堂の基本情報や特色、写真を掲載できます。<br>
                 実際に掲載されている、食堂をご覧ください。<br>
                 （例）<a href="<?php echo home_url('/cafeinfo/356/'); ?>">お結びコロコロ～お茶の間～</a>
@@ -23,6 +24,8 @@
             <p class="text">
                 このサイトは、完全ボランティアで運営しておりますので、レスポンスにお時間をいただきます。ご了承ください。
             </p>
+            <div class="house"></div>
+            <div class="vegetable"></div>
         </section>
     </div>
 </main>
