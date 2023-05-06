9f82ec560f7e1afcab6457ac67d174c0a0b053be
nozaki08 <uryahoooi89@gmail.com>
2023-05-06 16:29:14 +0900

    支援したい方へ
    支援したい方へ修正済
    
    野崎

--- page-support.php
+++ page-support.php
@@ -2,21 +2,24 @@
 
 <main>
     <div class="main_inner">
+        <div class="block"></div>
         <?php get_template_part('template-parts/breadcrumb'); ?>
         <h2 class="title">支援したい方へ</h2>
-        <p class="text">
-            こども食堂は、ほとんどがボランティアスタッフによって運営されています。<br>
-            行政等からの助成金がもらえる場合もありますが、それでは間に合わないことも多いです。<br>
-            自分にできることから、支援してみませんか？一言に”支援”と言っても、いくつかの方法があります。<br>
-            下記の3つのタブ、”物資”・”寄付”・”手伝い”から、自分に合った支援の方法を見つけてみてください。
-        </p>
+        <section class="section_inner">
+            <p class="support_inner text">
+                こども食堂は、ほとんどがボランティアスタッフによって運営されています。<br>
+                行政等からの助成金がもらえる場合もありますが、それでは間に合わないことも多いです。<br>
+                自分にできることから、支援してみませんか？一言に”支援”と言っても、いくつかの方法があります。<br>
+                下記の3つのタブ、”物資”・”寄付”・”手伝い”から、自分に合った支援の方法を見つけてみてください。
+            </p>
+        </section>
         <!-- 項目タブ -->
         <ul class="tab flex">
             <li class="tab_1 tab_js tab-a">物資</li>
             <li class="tab_2 tab_js tab-b">寄付</li>
             <li class="tab_3 tab_js tab-c">手伝い</li>
         </ul>
-        <div class="support_content">
+        <section class="section_inner support_content">
             <div class="support_supplies panel tab-a is-show">
                 <h3 class="subtitle support_subtitle01">
                     物資を支援する
@@ -28,7 +31,7 @@
                 <div class="font_awsome">
                     <i class="fa-solid fa-circle-arrow-down fa-bounce fa-lg" style="color: #23ac38"></i>
                 </div>
-                <div class="btn_item btn01">
+                <div class="support_btn_item btn01">
                     <a href="<?php echo home_url('programlist'); ?>">参加食堂一覧へ</a>
                 </div>
             </div>
@@ -43,7 +46,7 @@
                 <div class="font_awsome">
                     <i class="fa-solid fa-circle-arrow-down fa-bounce fa-lg" style="color: #23ac38"></i>
                 </div>
-                <div class="btn_item btn02">
+                <div class="support_btn_item btn02">
                     <a href="<?php echo home_url('area/east'); ?>">エリアからさがす</a>
                 </div>
             </div>
@@ -57,11 +60,11 @@
                 <div class="font_awsome">
                     <i class="fa-solid fa-circle-arrow-down fa-bounce fa-lg" style="color: #23ac38"></i>
                 </div>
-                <div class="btn_item btn03">
+                <div class="support_btn_item btn03">
                     <a href="<?php echo home_url('find'); ?>">条件からさがす</a>
                 </div>
             </div>
-        </div>
+        </section>
         <a href="<?php echo home_url('contact'); ?>">
             <div class="btn support_btn">
                 お問い合わせはこちら
