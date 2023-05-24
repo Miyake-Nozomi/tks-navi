c8ec7990b85dbc40ec5fdf108cb22f5c76997958
Miyake-Nozomi <128271935+Miyake-Nozomi@users.noreply.github.com>
2023-05-24 20:40:10 +0900

    ロゴデザインについてのページを仮作成

--- page-logo.php
+++ page-logo.php
@@ -1,3 +1,32 @@
 <?php get_header(); ?>
 
+<main>
+    <div class="main_inner">
+        <div class="block"></div>
+        <?php get_template_part('template-parts/breadcrumb'); ?>
+        <section class="concept section_inner">
+            <h2 class="title">ロゴデザインについて</h2>
+            <div class="textbox">
+                <div class="text">
+                    <p>
+                        テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                    </p>
+                </div>
+                <div class="text">
+                    <p>
+                        テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                    </p>
+                </div>
+                <div class="text">
+                    <p>
+                        テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                    </p>
+                </div>
+            </div>
+        </section>
+    </div>
+</main>
+
+
+
 <?php get_footer(); ?>
