edd033fbc834e8524c91e2444ed33bef0f5d3c94
nozaki08 <uryahoooi89@gmail.com>
2023-06-03 14:34:30 +0900

    link編集
    link編集済
    
    野崎

--- taxonomy-link.php
+++ taxonomy-link.php
@@ -29,9 +29,12 @@
         <h2 class="title">リンク集</h2>
         <!-- 項目タブ -->
         <ul class="tab flex">
-            <li class="tab_1 tab_js childcafe"><a class="tab_hover01" href="<?php echo home_url('/link/cafe'); ?>">こども食堂関連</a></li>
-            <li class="tab_2  tab_js childsupport"><a class="tab_hover02" href="<?php echo home_url('/link/care'); ?>">子育て支援関連</a></li>
-            <li class="tab_3  tab_js childplace"><a class="tab_hover03" href="<?php echo home_url('/link/third'); ?>">こども居場所関連</a></li>
+            <li class="tab_1 tab_js childcafe"><a class="tab_hover01"
+                    href="<?php echo home_url('/link/cafe'); ?>">こども食堂関連</a></li>
+            <li class="tab_2  tab_js childsupport"><a class="tab_hover02"
+                    href="<?php echo home_url('/link/care'); ?>">子育て支援関連</a></li>
+            <li class="tab_3  tab_js childplace"><a class="tab_hover03"
+                    href="<?php echo home_url('/link/third'); ?>">こども居場所関連</a></li>
         </ul>
         <!-- 関連リンク -->
         <section class="link_<?php echo $link->slug; ?>" panel is-show>
@@ -55,9 +58,11 @@
                 <?php wp_reset_postdata(); ?>
             </div>
             <?php if ($link->slug == 'cafe') : ?>
-            <img class="apple" src="<?php echo get_template_directory_uri(); ?>/assets/images/link/apple.png" alt="背景画像りんご" />
+            <img class="apple" src="<?php echo get_template_directory_uri(); ?>/assets/images/link/apple.png"
+                alt="背景画像りんご" />
             <?php elseif($link->slug == 'care') : ?>
-            <img class="girl" src="<?php echo get_template_directory_uri(); ?>/assets/images/link/girl.png" alt="背景女の子" />
+            <img class="yagi" src="<?php echo get_template_directory_uri(); ?>/assets/images/link/yagi.png"
+                alt="背景ヤギ" />
             <?php elseif($link->slug == 'third') : ?>
             <img class="veg" src="<?php echo get_template_directory_uri(); ?>/assets/images/link/veg.png" alt="背景野菜" />
             <?php endif; ?>
